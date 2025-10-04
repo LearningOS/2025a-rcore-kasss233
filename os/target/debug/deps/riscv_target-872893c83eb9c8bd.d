9d39d236f9cb7412cab60723d66be02f1315560a
kasss233 <1592858973@qq.com>
2025-10-04 14:11:42 +0800

    2

---
+++ os/target/debug/deps/riscv_target-872893c83eb9c8bd.d
@@ -0,0 +1,7 @@
+/home/kasss/2025a-rcore-kasss233/os/target/debug/deps/libriscv_target-872893c83eb9c8bd.rmeta: /home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/riscv-target-0.1.2/src/lib.rs
+
+/home/kasss/2025a-rcore-kasss233/os/target/debug/deps/libriscv_target-872893c83eb9c8bd.rlib: /home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/riscv-target-0.1.2/src/lib.rs
+
+/home/kasss/2025a-rcore-kasss233/os/target/debug/deps/riscv_target-872893c83eb9c8bd.d: /home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/riscv-target-0.1.2/src/lib.rs
+
+/home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/riscv-target-0.1.2/src/lib.rs:
---
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/log-3a2543d28cae9673.d
@@ -0,0 +1,8 @@
+/home/kasss/2025a-rcore-kasss233/os/target/riscv64gc-unknown-none-elf/debug/deps/liblog-3a2543d28cae9673.rmeta: /home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.28/src/lib.rs /home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.28/src/macros.rs /home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.28/src/serde.rs /home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.28/src/__private_api.rs
+
+/home/kasss/2025a-rcore-kasss233/os/target/riscv64gc-unknown-none-elf/debug/deps/log-3a2543d28cae9673.d: /home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.28/src/lib.rs /home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.28/src/macros.rs /home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.28/src/serde.rs /home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.28/src/__private_api.rs
+
+/home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.28/src/lib.rs:
+/home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.28/src/macros.rs:
+/home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.28/src/serde.rs:
+/home/kasss/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.28/src/__private_api.rs:
